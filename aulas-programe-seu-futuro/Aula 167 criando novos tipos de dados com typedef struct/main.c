4d160cbe271193b280a14b332c3f3b34396ee429
Marcos-Gundis <marcosgundis@gmail.com>
2025-08-18 15:19:38 -0300

    Update main.c

--- aulas-programe-seu-futuro/Aula 167 criando novos tipos de dados com typedef struct/main.c
+++ aulas-programe-seu-futuro/Aula 167 criando novos tipos de dados com typedef struct/main.c
@@ -6,12 +6,14 @@ Aula 167: Como criar novos tipos de dados em c com typedef struct
 #include <stdlib.h>
 #include <string.h>
 
+//Definindo o tipo de dado Pessoa  
 typedef struct{
     int idade;
     char sexo;
-    char nome[100];
+    char nome[100];  
 }Pessoa;
 
+//definindo o tipo de dado Pessoa2
 struct Pessoa2{
     int idade;
     char sexo;
@@ -20,17 +22,20 @@ struct Pessoa2{
 
 int main()
 {
+    //tipo de dado e variavel
     Pessoa pessoa1;
     struct Pessoa2 pessoa2;
     
     pessoa1.idade = 35;
     pessoa1.sexo = 'f';
+    //funcao que copia a string para variavel pessoa1
     strcpy(pessoa1.nome,"Maria Madalena"); 
     
     printf("Nome: %s\nIdade: %d\nSexo: %c\n", pessoa1.nome, pessoa1.idade, pessoa1.sexo);
     
     pessoa2.idade = 40;
     pessoa2.sexo = 'm';
+    //funcao que copia a string para variavel pessoa2
     strcpy(pessoa2.nome,"Jose da Silva"); 
     
     printf("Nome: %s\nIdade: %d\nSexo: %c\n", pessoa2.nome, pessoa2.idade, pessoa2.sexo);
