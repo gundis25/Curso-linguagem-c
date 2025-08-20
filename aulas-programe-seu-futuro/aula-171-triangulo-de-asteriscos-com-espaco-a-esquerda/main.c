898bee2a2be1557c387e4bdca6b22020cde5d146
Marcos-Gundis <marcosgundis@gmail.com>
2025-08-20 14:30:22 -0300

    commit new execise

---
+++ aulas-programe-seu-futuro/aula-171-triangulo-de-asteriscos-com-espaco-a-esquerda/main.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Aula 171: Imprimindo um triangulo de asteriscos com espaços a esquerda
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j, n;
+    
+    printf("Digite o valor de n: ");
+    scanf("%d", &n);
+    
+    for(i = 1; i <= n; i++){
+        for(j = n - i; j >= 1; j--)
+            printf(" ");
+        for(j = 1; j <= i; j++)
+            printf("*");
+        printf("\n");    
+    }
+
+    return 0;
+}
---
+++ aulas-programe-seu-futuro/aula-174-criando-um-vetor-array-unidimencional-de-struct/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+            Aula 174: Criaando um vetor de struct.
+
+*/
+
+typedef struct{
+    int dia, mes, ano;
+}DataNas;
+
+typedef struct{
+    DataNas dataNas;
+    int idade;
+    char sexo;
+    char nome[100];
+}Pessoa;
+
+// Procedimento para imprimir os dados de uma Pessoa
+void imprimirPessoa(Pessoa p){
+    printf("\n\tNome: %s", p.nome);
+    printf("\tIdade: %d\n", p.idade);
+    printf("\tSexo: %c\n", p.sexo);
+    printf("\tData de nas.: %d/%d/%d\n\n", p.dataNas.dia, p.dataNas.mes, p.dataNas.ano);
+}
+
+// função que lê os dados de uma pessoa e retorna para quem chamou
+Pessoa lerPessoa(){
+    Pessoa p;
+    printf("\nDigite seu nome: ");
+    fgets(p.nome, 100, stdin);
+    printf("Digite sua idade: ");
+    scanf("%d", &p.idade);
+    scanf("%c");
+    printf("Digite f ou m para o sexo:");
+    scanf("%c", &p.sexo);
+    printf("Digite sua data de nascimento no formato dd mm aaaa:");
+    scanf("%d%d%d", &p.dataNas.dia, &p.dataNas.mes, &p.dataNas.ano);
+    scanf("%c");
+    return p;
+}
+
+int main() {
+    int i;
+    Pessoa pessoas[3];
+
+    for(i = 0; i < 3; i++)
+        pessoas[i] = lerPessoa();
+
+    for(i = 0; i < 3; i++)
+        imprimirPessoa(pessoas[i]);
+
+    return 0;
+}
