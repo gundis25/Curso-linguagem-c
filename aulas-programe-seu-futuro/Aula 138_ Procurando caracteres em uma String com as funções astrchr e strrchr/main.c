938e5e39fb6f77594f6eec82e1e7c00fb76c3823
Marcos-Gundis <marcosgundis@gmail.com>
2025-12-09 12:30:29 -0300

    função strchr e strrchr

--- aulas-programe-seu-futuro/Aula 138_ Procurando caracteres em uma String com as funções astrchr e strrchr/main.c
+++ aulas-programe-seu-futuro/Aula 138_ Procurando caracteres em uma String com as funções astrchr e strrchr/main.c
@@ -15,7 +15,7 @@ int main(){
     char palavra[50] = {"abacate"};
     char *letra;
 
-    // primeira ocorrência da letra a 
+    // função que retorna a primeira ocorrência da letra a 
     letra = strchr(palavra, 'a');
 
     printf("\n%c\n", *letra); // imprime a letra a
@@ -24,6 +24,7 @@ int main(){
     printf("\n%c\n", *(letra + 1)); // imprime a letra b
     printf("\n%c\n", *(letra + 2)); // imprime a segunda letra a
 
+    // função que retorna a última ocorrência da letra a 
     letra = strrchr(palavra, 'a');
 
     printf("\n%c\n", *letra); // imprime a última letra a
